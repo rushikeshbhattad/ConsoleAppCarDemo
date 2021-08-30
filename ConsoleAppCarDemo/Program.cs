3f8727d079b14de04bce1dda3fa296f123ddff49
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 15:34:29 +0530

    Add project files.

---
+++ ConsoleAppCarDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCarDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car Ford = new Car(22);
+            Ford.Model = 10;
+            Car BMW = Ford;
+            BMW.Display_Model();
+            Ford.Display_Model();
+        }
+    }
+
+    class Car
+    {
+        public int Model;
+        public Car(int a)
+        {
+            Model = a;
+        }
+        public void Display_Model()
+        {
+            Console.WriteLine(Model);
+            Console.ReadKey();
+        }
+    }
+
+}
